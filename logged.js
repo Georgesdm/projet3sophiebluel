4d0f6c570b1bd6033b26ca9c8a8c9034f7289526
georges <118487336+Georgesdm@users.noreply.github.com>
2024-07-31 22:59:53 +0200

    gestion affichage barre noire

--- logged.js
+++ logged.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const loginLink = document.getElementById('loginLink');
     const logoutLink = document.getElementById('logoutLink');
     const editAdminButton = document.getElementById('editAdminButton');
+    const editModeBar = document.getElementById('editModeBar');
+    const header = document.querySelector('header');
     
     console.log('loginLink:', loginLink);
     console.log('logoutLink:', logoutLink);
@@ -18,12 +20,16 @@ document.addEventListener('DOMContentLoaded', function() {
             loginLink.style.display = 'none'; //enlève login
             logoutLink.style.display = 'block'; //display logout
             editAdminButton.style.display = 'flex';
+            editModeBar.style.display = 'block'; // Affiche la barre de notification
+            header.style.marginTop = '99px'; // Réinitialise la marge supérieure du header
 
         } else {
             console.log('Aucun utilisateur connecté');
             loginLink.style.display = 'block'; //display lelogin
             logoutLink.style.display = 'none'; //enlève logout
             editAdminButton.style.display = 'none';
+            editModeBar.style.display = 'none'; // Cache la barre de notification
+            header.style.marginTop = '50px 0'; // Réinitialise la marge supérieure du header
         }
     }
 
